7147df5d115d077c9f31f2c411c4639dfb4a7d93
pinkdawn3 <albadetaoro@gmail.com>
2026-01-09 17:49:21 +0000

    first test

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+# Naming conventions for tests -> test_ and whatever it comes next
+
+import unittest
+import calc 
+
+class TestCalc(unittest.TestCase):
+
+    # If the method doesn't start with test_ it won't run
+    def test_add(self):
+        result = calc.add(10, 5)
+        self.assertEqual(result, 15)
