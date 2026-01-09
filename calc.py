b146255d292ddebbba87d8b8e72d92ea9f60ef61
pinkdawn3 <albadetaoro@gmail.com>
2026-01-09 18:17:16 +0000

    feat: added test that checks error

--- calc.py
+++ calc.py
@@ -1,3 +1,11 @@
 def add(x, y):
     '''Add Function'''
     return x + y
+
+
+def divide(x, y):
+    '''Divide Function'''
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+
+    return x / y
--- test_calc.py
+++ test_calc.py
@@ -7,9 +7,23 @@ class TestCalc(unittest.TestCase):
 
     # If the method doesn't start with test_ it won't run
     def test_add(self):
-        result = calc.add(10, 5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10, 5), 15)
+
+        # Let's test edge cases with negative numbers
+        self.assertEqual(calc.add(-1, 1), 0)
+        self.assertEqual(calc.add(-1, -1), -2)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10, 5), 2)
+        self.assertEqual(calc.divide(-1, 1), -1)
+        self.assertEqual(calc.divide(-1, -1), 1)
+        self.assertEqual(calc.divide(5, 2), 2.5)
+
+        # param: result, function, parameters
+        self.assertRaises(ValueError, calc.divide, 10, 0)
+        
 
 
+# Conditional so instead of running the python file, it runs through the unittest module
 if __name__ == '__main__':
     unittest.main()
